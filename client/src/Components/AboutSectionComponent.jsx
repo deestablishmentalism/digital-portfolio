144c91bb8017e7376bd047b766ec69b3c6328d4c
deestablismentalism <davidiyad6@gmail.com>
2026-08-09 16:34:20 +0800

    Updated gitignore to untrack the dist folder

--- client/src/Components/AboutSectionComponent.jsx
+++ client/src/Components/AboutSectionComponent.jsx
@@ -18,18 +18,18 @@ export default function AboutSectionComponent({preview=false}) {
     return(
         <>
             <div>
-                <div className="p-2 flex">
-                    <div className="flex flex-col w-50 mr-2">
+                <div className="p-2 flex gap-2 flex-wrap md:flex-nowrap">
+                    <div className="flex flex-col w-full mr-2 md:w-50">
                         {personalInfo?.profile  && (
-                            <div className="rounded overflow-hidden w-50 h-40">
+                            <div className="rounded overflow-hidden w-full h-70 md:w-50 md-h-60">
                             {
                                 <img src={personalInfo?.profile?.secure_url} alt="profile"/>
                             }
                             </div>
                         )}    
-                        <div className="mt-2 px-1 rounded-full text-center border-2 border-slate-500 bg-gradient-to-r from-teal-900/70 via-teal-600/70 to-teal-950/70">
+                        <div className="mt-2 px-1 rounded-full text-center border-2 border-slate-500 bg-radial from-teal-600/70 via-teal-800/70 to-teal-900/70">
                             {personalInfo?.first_name && personalInfo?.middle_name && personalInfo?.last_name && (
-                                <span className="text-white text-[11px] font-header-text">{personalInfo?.last_name}, {personalInfo?.first_name} {personalInfo?.middle_name}</span>
+                                <span className="text-teal-200 text-[11px] font-header-text">{personalInfo?.last_name}, {personalInfo?.first_name} {personalInfo?.middle_name}</span>
                             ) 
                             }
                         </div>
@@ -38,7 +38,7 @@ export default function AboutSectionComponent({preview=false}) {
                         {personalInfo?.address && (
                             <div>
                                 <h1 className="font-sub-header-text text-white mb-2">Address</h1>
-                                <div className="px-2 py-1 w-fit rounded-full border-2 border-slate-500 bg-gradient-to-r from-teal-900/70 via-teal-600/70 to-teal-950/70">
+                                <div className="px-2 py-1 w-fit text-[11px] text-center rounded-full border-2 border-slate-500 bg-gradient-to-r from-teal-900/70 via-teal-600/70 to-teal-950/70">
                                     <span className="text-white font-bold text-[11px] ">
                                         {personalInfo?.address.region}, {personalInfo?.address.house_number} {personalInfo?.address.subdivision}, {personalInfo?.address.barangay},
                                         {personalInfo?.address.city}, {personalInfo?.address.province}
@@ -49,9 +49,9 @@ export default function AboutSectionComponent({preview=false}) {
                         <div>
                             <h1 className="font-sub-header-text text-white mb-2">Summary</h1>
                             {personalInfo?.summary && (
-                                <div>
+                                <p className="text-[13px] text-white font-bold">
                                     {personalInfo?.summary}
-                                </div>
+                                </p>
                             )
                             }
                         </div>
