437e8f84c65b8b11eecd82776eef795bed67433b
deestablismentalism <davidiyad6@gmail.com>
2026-08-09 16:31:00 +0800

    removed dist folder from repo

--- client/dist/assets/Dashboard-Cl7bh0Sa.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./jsx-runtime-DUAcabCT.js";var t=e();function n(){return(0,t.jsx)(t.Fragment,{children:(0,t.jsx)(`div`,{children:(0,t.jsx)(`h1`,{children:` This is dashboard`})})})}export{n as default};
